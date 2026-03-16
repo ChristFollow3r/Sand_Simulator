583501dbe635fa5489ad54b4ef229c4ad3f1d700
Nil <nil.badia@estudiant.enti.cat>
2026-03-16 20:33:23 +0100

    Refactored sand grain code using a lambda. Holy shit lambdas are usefull af!

--- Sand Simulator/Sand Simulator/sandGrain.cpp
+++ Sand Simulator/Sand Simulator/sandGrain.cpp
@@ -3,76 +3,46 @@
 void SandGrain::ApplyPhysics(Block(&grid)[gridSize]) {
 
 	int random = rand() % 101;
-	//int boundry = sandGrain->gridIndex % 80;
 
-	if (this == nullptr) return;
+	auto moveTo = [&](int targetPosition) {
 
-	if (this->gridIndex + cols >= gridSize) return;
-
-	if (grid[this->gridIndex + cols].type == AirType) {
-		grid[this->gridIndex].type = AirType;
-		grid[this->gridIndex].materialPointer = nullptr;
-		grid[this->gridIndex + cols].type = SandType;
-		grid[this->gridIndex + cols].materialPointer = shared_from_this();
-		this->rect = grid[this->gridIndex + cols].rect;
-		this->gridIndex += cols;
-	}
-
-	else if (grid[this->gridIndex + cols].type == WaterType) {
-		grid[this->gridIndex].type = WaterType;
-		grid[this->gridIndex].materialPointer = grid[this->gridIndex + cols].materialPointer;
-		grid[this->gridIndex].materialPointer->gridIndex = this->gridIndex;
-		grid[this->gridIndex].materialPointer->rect = grid[this->gridIndex].rect;
-
-		grid[this->gridIndex + cols].type = SandType;
-		grid[this->gridIndex + cols].materialPointer = shared_from_this();
-		this->rect = grid[this->gridIndex + cols].rect;
-		this->gridIndex += cols;
-	}
-
-	else if (grid[this->gridIndex + cols].type != AirType) { // Change this eventually so it can go through water
-
-		bool leftFree = grid[this->gridIndex + cols - 1].type == AirType || grid[this->gridIndex + cols - 1].type == WaterType;
-		bool rightFree = grid[this->gridIndex + cols + 1].type == AirType || grid[this->gridIndex + cols + 1].type == WaterType;
-
-
-		if (leftFree && rightFree) {
-			if (random <= 50) {
-				grid[this->gridIndex].type = AirType;
-				grid[this->gridIndex].materialPointer = nullptr;
-				grid[this->gridIndex + cols - 1].type = SandType;
-				grid[this->gridIndex + cols - 1].materialPointer = shared_from_this();
-				this->rect = grid[this->gridIndex + cols - 1].rect;
-				this->gridIndex += cols - 1;
-				return;
-			}
-			else if (random > 50) {
-				grid[this->gridIndex].type = AirType;
-				grid[this->gridIndex].materialPointer = nullptr;
-				grid[this->gridIndex + cols + 1].type = SandType;
-				grid[this->gridIndex + cols + 1].materialPointer = shared_from_this();
-				this->rect = grid[this->gridIndex + cols + 1].rect;
-				this->gridIndex += cols + 1;
-				return;
-			}
+		if (targetPosition < 0 || targetPosition >= gridSize) return;
+		if (grid[targetPosition].type == WaterType) {
+			grid[this->gridIndex].type = grid[targetPosition].type; // It gets its type
+			grid[this->gridIndex].materialPointer = grid[targetPosition].materialPointer; // It becomes the thing that wants to go to
+			grid[this->gridIndex].materialPointer->gridIndex = this->gridIndex; // It gets its index
+			grid[this->gridIndex].materialPointer->rect = grid[this->gridIndex].rect; // It gets its rect
 		}
-		else if (leftFree) {
+		else {
 			grid[this->gridIndex].type = AirType;
 			grid[this->gridIndex].materialPointer = nullptr;
-			grid[this->gridIndex + cols - 1].type = SandType;
-			this->rect = grid[this->gridIndex + cols - 1].rect;
-			this->gridIndex += cols - 1;
-			return;
+		}
+		grid[targetPosition].type = SandType;
+		grid[targetPosition].materialPointer = shared_from_this();
+		this->rect = grid[targetPosition].rect;
+		this->gridIndex = targetPosition;
+
+		};
+
+	if (gridIndex + cols >= gridSize) return;
+
+	if (grid[this->gridIndex + cols].type == AirType || grid[this->gridIndex + cols].type == WaterType) moveTo(this->gridIndex + cols);
+
+	else {
+
+		bool leftfree = grid[this->gridIndex + cols - 1].type == AirType || grid[this->gridIndex + cols - 1].type == WaterType;
+		bool rightfree = grid[this->gridIndex + cols + 1].type == AirType || grid[this->gridIndex + cols + 1].type == WaterType;
+
+		if (leftfree && rightfree) {
+			if (random <= 50) moveTo(this->gridIndex + cols - 1);
+			else moveTo(this->gridIndex + cols + 1);
 		}
 
-		else if (rightFree) {
-			grid[this->gridIndex].type = AirType;
-			grid[this->gridIndex].materialPointer = nullptr;
-			grid[this->gridIndex + cols + 1].type = SandType;
-			this->rect = grid[this->gridIndex + cols + 1].rect;
-			this->gridIndex += cols + 1;
-			return;
-		}
+		else if (leftfree) moveTo(this->gridIndex + cols - 1);
+		else if (rightfree) moveTo(this->gridIndex + cols + 1);
+
 	}
 
 }
+
+
