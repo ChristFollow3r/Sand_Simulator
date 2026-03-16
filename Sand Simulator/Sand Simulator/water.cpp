ce5d442fe6524c387379ece959d3c2e855b44ab5
Nil <nil.badia@estudiant.enti.cat>
2026-03-16 21:02:50 +0100

    Did the lambda for the Water class.

--- Sand Simulator/Sand Simulator/water.cpp
+++ Sand Simulator/Sand Simulator/water.cpp
@@ -2,98 +2,31 @@
 
 void Water::ApplyPhysics(Block (&grid)[gridSize]) {
 
+	auto moveTo = [&](int targetPosition) {
+		grid[this->gridIndex].type = AirType;
+		grid[this->gridIndex].materialPointer = nullptr;
+		grid[targetPosition].type = WaterType;
+		grid[targetPosition].materialPointer = shared_from_this();
+		this->rect = grid[targetPosition].rect;
+		this->gridIndex = targetPosition;
+	};
+
 	int random = rand() % 101;
-
 	if (this == nullptr) return;
+	if (this->gridIndex + cols >= gridSize) return;
 
-	if (this->gridIndex + cols >= gridSize) return; //Prevents nullpointer acces error
+	if (grid[this->gridIndex + cols].type == AirType) { moveTo(this->gridIndex + cols); return; }
 
-	if (grid[this->gridIndex + 1].type != AirType
-		&& grid[this->gridIndex - 1].type != AirType && grid[this->gridIndex + cols + 1].type != AirType && grid[this->gridIndex + cols - 1].type != AirType) return;
+	int col = this->gridIndex % cols;
 
-	if (grid[this->gridIndex + cols].type == AirType) { // Falls 
-		grid[this->gridIndex].type = AirType;
-		grid[this->gridIndex].materialPointer = nullptr;
-		grid[this->gridIndex + cols].type = Type::WaterType;
-		grid[this->gridIndex + cols].materialPointer = shared_from_this();
-		this->rect = grid[this->gridIndex + cols].rect;
-		this->gridIndex += cols;
-		return;
+	if (random > 50) {
+		if (grid[(this->gridIndex + cols) + 1].type == AirType && col > 0) { moveTo((this->gridIndex + cols) + 1); return; }
+		if (grid[(this->gridIndex + cols) - 1].type == AirType && col < cols - 1) { moveTo((this->gridIndex + cols) - 1); return; }
 	}
 
-	if (this->gridIndex + cols == gridSize) {
-		if (random > 50) {
-			if (this->gridIndex + 1 >= gridSize) return;
-			grid[this->gridIndex].type = AirType;
-			grid[this->gridIndex].materialPointer = nullptr;
-			grid[this->gridIndex + 1].type = Type::WaterType;
-			grid[this->gridIndex + 1].materialPointer = shared_from_this();
-			this->rect = grid[this->gridIndex + 1].rect;
-			this->gridIndex += 1;
-		}
-		else {
-			if (this->gridIndex - 1 < 0) return;
-			grid[this->gridIndex].type = AirType;
-			grid[this->gridIndex].materialPointer = nullptr;
-			grid[this->gridIndex - 1].type = Type::WaterType;
-			grid[this->gridIndex - 1].materialPointer = shared_from_this();
-			this->rect = grid[this->gridIndex - 1].rect;
-			this->gridIndex -= 1;
-		}
- 	}
-	
-
-	if (this->gridIndex + (cols - 1) < 0 || this->gridIndex + (cols + 1) >= gridSize) return; //Prevents mullpointer acces error
-
-	else if (grid[(this->gridIndex + cols) - 1].type != AirType && grid[(this->gridIndex + cols) + 1].type == AirType) { // Move down right
-		grid[this->gridIndex].type = AirType;
-		grid[this->gridIndex].materialPointer = nullptr;
-		grid[(this->gridIndex + cols) + 1].type = Type::WaterType;
-		grid[(this->gridIndex + cols) + 1].materialPointer = shared_from_this();
-		this->rect = grid[(this->gridIndex + cols) + 1].rect;
-		this->gridIndex += (cols + 1);
-		return;
-	}
-
-	else if (grid[(this->gridIndex + cols) + 1].type != AirType && grid[(this->gridIndex + cols) - 1].type == AirType) { // Move down left
-		grid[this->gridIndex].type = AirType;
-		grid[this->gridIndex].materialPointer = nullptr;
-		grid[(this->gridIndex + cols) - 1].type = Type::WaterType;
-		grid[(this->gridIndex + cols) - 1].materialPointer = shared_from_this();
-		this->rect = grid[(this->gridIndex + cols) - 1].rect;
-		this->gridIndex += (cols - 1);
-		return;
-	}
-
-	else  { // Move left or right
-
-		if (random > 50) {
-
-			if (this->gridIndex + 1 >= gridSize || grid[this->gridIndex + 1].type != AirType) return;
-			else {
-				grid[this->gridIndex].type = AirType;
-				grid[this->gridIndex].materialPointer = nullptr;
-				grid[this->gridIndex + 1].type = Type::WaterType;
-				grid[this->gridIndex + 1].materialPointer = shared_from_this();
-				this->rect = grid[this->gridIndex + 1].rect;
-				this->gridIndex += 1;
-				return;
-			}
-		}
-
-		else {
-
-			if (this->gridIndex - 1 < 0 || grid[this->gridIndex - 1].type != AirType) return;
-			else {
-				grid[this->gridIndex].type = AirType;
-				grid[this->gridIndex].materialPointer = nullptr;
-				grid[this->gridIndex - 1].type = Type::WaterType;
-				grid[this->gridIndex - 1].materialPointer = shared_from_this();
-				this->rect = grid[this->gridIndex - 1].rect;
-				this->gridIndex -= 1;
-				return;
-			}
-		}
+	else {
+		if (grid[this->gridIndex + 1].type == AirType && col < cols - 1) { moveTo(this->gridIndex + 1); return; }
+		if (grid[this->gridIndex - 1].type == AirType && col > 0) { moveTo(this->gridIndex - 1); return; }
 	}
 
 }
