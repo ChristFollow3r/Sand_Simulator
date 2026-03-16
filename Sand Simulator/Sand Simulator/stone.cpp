ee23de76955a30100325e58c866be62f433fbba3
Nil <nil.badia@estudiant.enti.cat>
2026-03-16 14:36:58 +0100

    Now you can delete stone.

--- Sand Simulator/Sand Simulator/stone.cpp
+++ Sand Simulator/Sand Simulator/stone.cpp
@@ -1,5 +1,8 @@
 #include "stone.h"
 
 void Stone::ApplyPhysics(Block (&grid)[gridSize]) {
+	this->rect = grid[this->gridIndex].rect;
+	grid[this->gridIndex].materialPointer = shared_from_this();
+	grid[this->gridIndex].type = StoneType;
 	return;
 }
